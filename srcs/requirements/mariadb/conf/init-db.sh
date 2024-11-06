8e02743c15146106c1ac71d4fde1e659898e2d0e
mamoud <mamoud.antacid895@passmail.com>
2024-11-06 17:35:48 +0100

    fix: replace mariadb starting service by safe starting

--- srcs/requirements/mariadb/conf/init-db.sh
+++ srcs/requirements/mariadb/conf/init-db.sh
@@ -2,31 +2,32 @@
 echo "Starting init-db.sh script..."
 
 # Démarrer le service MariaDB
-service mariadb start
-# Attendre que le serveur MariaDB soit pleinement opérationnel
-until mysqladmin ping --silent; do
-    sleep 2
+exec mysqld_safe &
+
+until mysqladmin ping >/dev/null 2>&1; do
+    sleep 1
+    echo "Waiting for MariaDB service..."
 done
 echo "MariaDB service started."
 
 # Créer la base de données
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -h localhost -u root -p${SQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 echo "Database created."
 
 # Créer l'utilisateur
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -h localhost -u root -p${SQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 echo "User created."
 
 # Attribuer les privilèges à l'utilisateur
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -h localhost -u root -p${SQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';" 
 echo "Privileges granted."
 
 # Mettre à jour le mot de passe root
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -h localhost -u root -p${SQL_ROOT_PASSWORD} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 echo "Root password updated."
 
 # Rafraîchir les privilèges
-mysql -e "FLUSH PRIVILEGES;"
+mysql -h localhost -u root -p${SQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 echo "Privileges flushed."
 
 # Arrêter le serveur MySQL de façon contrôlée avec le mot de passe root
